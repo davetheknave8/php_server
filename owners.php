c3303bb441cbce9fa92cdbe21a36f711ad0fc0e8
David Reeves <davidsreeves1@gmail.com>
2019-08-21 14:28:07 -0500

    first commit php server

---
+++ owners.php
@@ -0,0 +1,18 @@
+<?php
+//Native code to use PostgreSQL with PHP
+    $con = pg_connect("host=localhost dbname=php_react") or die("Could not connect to Server\n);");
+    //Query to get info from person table in database
+    $results = pg_query($con, "SELECT \"owner\".\"name\", count(pets) FROM \"owner\"
+	JOIN pets ON \"owner\".id = pets.owner_id
+	GROUP BY \"owner\".\"name\";") or die('Query failed: ' . pg_last_error());
+
+    //Setting results from query to $all_results
+    $all_results = pg_fetch_all($results);
+
+    //Looping through $all_results and displaying on DOM
+    $myJSON = json_encode($all_results);
+
+    echo $myJSON;
+
+     //Closing the connection
+    pg_close($con);
