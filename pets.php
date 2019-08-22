101d59f9fcbc21979c2ef28497518231c55d1fcd
Kyle Olson <kyleolson87@Kyles-MacBook-Pro-3.local>
2019-08-22 09:45:57 -0500

    made changes to SQL Query in pets.php

--- pets.php
+++ pets.php
@@ -3,7 +3,8 @@
     $con = pg_connect("host=localhost dbname=php_react") or die("Could not connect to Server\n);");
     //Query to get info from person table in database
     $results = pg_query($con, "SELECT \"pets\".\"id\", \"owner_id\", \"name\", \"pet_name\", \"breed\", \"color\", \"checked_in\"  FROM \"pets\"
-    JOIN \"owner\" on \"owner\".\"id\" = \"owner_id\";") or die('Query failed: ' . pg_last_error());
+    JOIN \"owner\" on \"owner\".\"id\" = \"owner_id\";
+    ") or die('Query failed: ' . pg_last_error());
 
     //Setting results from query to $all_results
     $all_results = pg_fetch_all($results);
