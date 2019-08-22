cd01cc395e6795d75d1ab266e0c8bb1bbef99417
David Reeves <davidsreeves1@gmail.com>
2019-08-22 09:24:59 -0500

    added addowner php file

---
+++ addOwner.php
@@ -0,0 +1,15 @@
+<?php
+//Native code to use PostgreSQL with PHP
+    $con = pg_connect("host=localhost dbname=php_react") or die("Could not connect to Server\n);");
+    
+    $name = pg_escape_string($_POST[adam]);
+
+    $result = pg_query($con, "INSERT INTO owner (name) VALUES('$name')");
+    if( !$result) {
+        die("Error in SQL query: " . pg_last_error());
+    }
+
+    echo $name;
+    echo 'hellot';
+
+    pg_close($con);
