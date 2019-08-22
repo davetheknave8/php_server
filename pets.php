c8e64c02743a19b6d3cb56edd60053b253bbed15
Kyle Olson <kyleolson87@Kyles-MacBook-Pro-3.local>
2019-08-22 09:40:54 -0500

    made sql change in pets.php

--- pets.php
+++ pets.php
@@ -2,7 +2,8 @@
 //Native code to use PostgreSQL with PHP
     $con = pg_connect("host=localhost dbname=php_react") or die("Could not connect to Server\n);");
     //Query to get info from person table in database
-    $results = pg_query($con, "SELECT * FROM pets;") or die('Query failed: ' . pg_last_error());
+    $results = pg_query($con, "SELECT \"pets\".\"id\", \"owner_id\", \"name\", \"pet_name\", \"breed\", \"color\", \"checked_in\"  FROM \"pets\"
+    JOIN \"owner\" on \"owner\".\"id\" = \"owner_id\";") or die('Query failed: ' . pg_last_error());
 
     //Setting results from query to $all_results
     $all_results = pg_fetch_all($results);
