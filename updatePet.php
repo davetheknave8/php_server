7a0476958ab1fc49039a74ca149da6f6d923ef0e
Kyle Olson <kyleolson87@Kyles-MacBook-Pro-3.local>
2019-08-23 06:08:18 -0500

    built updatePet.php and able to update pets

---
+++ updatePet.php
@@ -0,0 +1,25 @@
+<?php
+//Native code to use PostgreSQL with PHP
+    $con = pg_connect("host=localhost dbname=php_react") or die("Could not connect to Server\n);");
+   
+   
+    // $name = json_decode($_POST["name"])
+    $idToUpdate = $_GET["id"];
+
+    $json = file_get_contents('php://input');
+
+    $data = json_decode($json);
+
+    $pet_name = $data->pet_name;
+    $breed = $data->breed;
+    $color = $data->color;
+
+
+    $result = pg_query($con, "UPDATE pets SET pet_name='$pet_name', breed='$breed' , color='$color' WHERE id=$idToUpdate");
+    if( !$result) {
+        die("Error in SQL query: " . pg_last_error());
+    }
+
+    echo $name;
+
+    pg_close($con);
